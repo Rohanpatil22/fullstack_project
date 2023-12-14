8b90e359b853b411c09a9be231688228b976784a
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-12-14 23:13:55 +0530

    login page form validation added and datepicker added for date selection

--- client/table_booking_app/src/components/BookingData.js
+++ client/table_booking_app/src/components/BookingData.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 function BookingData(props) {
+
+    const[selDate,setSelDate]=useState('');
   return (
     <>
         <div class="w-[500px] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
@@ -23,7 +27,7 @@ function BookingData(props) {
 
        <div>
             <label for="date" class="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Select Date</label>
-            <input type="date" name="date" id="date" placeholder="••••••••" class="w-[430px] bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  />
+            <DatePicker className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[420px] p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" minDate={new Date(Date.now())} onChange={(date)=>{setSelDate(date)}} selected={selDate} dateFormat="dd-MM-yyyy"/>
         </div>
 
         <div className='w-full m-auto text-center'> <button class="w-[300px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-auto" >Book your Table</button></div>
--- client/table_booking_app/src/components/Login.js
+++ client/table_booking_app/src/components/Login.js
@@ -1,43 +1,71 @@
 import React, { useState } from 'react';
 import bgImg from '../images/bg_img_2.jpg'
 import axios from 'axios';
+import {useForm} from 'react-hook-form';
+import{toast,Toaster} from 'react-hot-toast'
 
 function Login() {
 
     const [loginData,setloginData]=useState({email:"",password:""});
+    const{register,handleSubmit,formState:{errors}}=useForm();
+
+    const onErrors= (data)=>{console.log(data)};
+
     const loginUser=async()=>{
 
        await axios.post("/login",loginData)
        .then((res)=>{
 
           console.log(res);
+
+          if(res.data.Success)
+            {
+                toast.success(res.data.msg);
+            }
+            else{
+
+                toast.error(res.data.msg);
+            }
        })
 
     }
   return (
     <>
+    <div>
+        <Toaster  
+         toastOptions={{
+         className: '',
+         style: {
+         border: '1px solid #713200',
+         padding: '20px',
+         color: '#713200',
+         fontSize:"22px",
+      
+     },
+  }}/></div>
     <div className="m-auto pt-40" style={{backgroundImage:`url(${bgImg})`,width:"100%",height:"1400px",backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
     <div className='w-2/5 m-auto bg-stone-400 p-20 rounded-3xl' >
+        <form onSubmit={handleSubmit(loginUser,onErrors)}>
            <table className='text-2xl m-auto'>
             <tbody>
                   
                 <tr>
                     <td className='p-3'><label htmlFor='email_inp' className='font-medium'>Email Id</label></td> 
-                    <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="email_inp" autoFocus onChange={(e)=>{setloginData((prev)=>{return{...prev,email:e.target.value}})}} /></td>
+                    <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="email_inp" name="email_inp" autoFocus  {...register('email_inp',{required:"Email is required."})} onChange={(e)=>{setloginData((prev)=>{return{...prev,email:e.target.value}})}} /><br/><small className='text-red-700'>{errors?.email_inp && errors.email_inp.message}</small></td>
                 </tr>
 
                 <tr >
                     <td className='p-3'><label htmlFor='pass_inp' className='font-medium'>Password</label></td>
-                    <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="pass_inp" onChange={(e)=>{setloginData((prev)=>{return{...prev,password:e.target.value}})}}  /></td>
+                    <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="pass_inp" name="pass_inp" {...register('pass_inp',{required:"Password is required."})} onChange={(e)=>{setloginData((prev)=>{return{...prev,password:e.target.value}})}}  /><br/><small className='text-red-700'>{errors?.pass_inp && errors.pass_inp.message}</small></td>
                 </tr>
 
                 <tr>
-                    <td className='p-3 text-center pt-10' colSpan={2}><button className='p-3 w-40 bg-blue-900 rounded-xl text-white font-medium hover:scale-110 cursot-pointer mr-2' type="reset">Reset</button><button className='p-3 ml-3 w-40 bg-teal-800 text-white font-medium rounded-xl hover:scale-110 cursot-pointer' onClick={loginUser}>Submit</button></td>
+                    <td className='p-3 text-center pt-10' colSpan={2}><button className='p-3 w-40 bg-blue-900 rounded-xl text-white font-medium hover:scale-110 cursot-pointer mr-2' type="reset">Reset</button><button className='p-3 ml-3 w-40 bg-teal-800 text-white font-medium rounded-xl hover:scale-110 cursot-pointer' >Submit</button></td>
                     
                 </tr>
             </tbody>
            </table>
-                
+           </form>
           
 
            
