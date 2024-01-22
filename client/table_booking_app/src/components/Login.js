b3247aad9462783afc41b921349198b89ba3b598
Rohanpatil22 <rohanpatil1797@gmail.com>
2024-01-22 22:51:24 +0530

    signup and login component made mobile reponsive

--- client/table_booking_app/src/components/Login.js
+++ client/table_booking_app/src/components/Login.js
@@ -57,24 +57,24 @@ function Login() {
       
      },
   }}/></div>
-    <div className="m-auto pt-40" style={{backgroundImage:`url(${bgImg})`,width:"100%",height:"1400px",backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
-    <div className='w-2/5 m-auto bg-stone-400 p-20 rounded-3xl' >
+    <div className="mx-auto md:pt-40 sm:pt-10 w-full md:h-[1400px] sm:h-[570px]" style={{backgroundImage:`url(${bgImg})`,width:"100%",backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
+    <div className='md:w-2/5 sm:w-[90%] mx-auto bg-stone-400 md:p-20 sm:p-4 rounded-3xl' >
         <form onSubmit={handleSubmit(loginUser,onErrors)}>
            <table className='text-2xl m-auto'>
             <tbody>
                   
                 <tr>
-                    <td className='p-3'><label htmlFor='email_inp' className='font-medium'>Email Id</label></td> 
-                    <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="email_inp" name="email_inp" value={loginData.email} autoFocus  {...register('email_inp',{required:"Email is required."})} onChange={(e)=>{setloginData((prev)=>{return{...prev,email:e.target.value}})}} /><br/><small className='text-red-700'>{errors?.email_inp && errors.email_inp.message}</small></td>
+                    <td className='md:p-3 sm:p-1'><label htmlFor='email_inp' className='font-medium md:text-2xl sm:text-lg'>Email Id</label></td> 
+                    <td className='md:p-3 sm:p-1'><input className='rounded-xl p-1 md:p-1 sm:p-0 md:w-[400px] sm:w-[200px] text-black' type="text" id="email_inp" name="email_inp" value={loginData.email} autoFocus  {...register('email_inp',{required:"Email is required."})} onChange={(e)=>{setloginData((prev)=>{return{...prev,email:e.target.value}})}} /><br/><small className='text-red-700'>{errors?.email_inp && errors.email_inp.message}</small></td>
                 </tr>
 
                 <tr >
-                    <td className='p-3'><label htmlFor='pass_inp' className='font-medium'>Password</label></td>
-                    <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="pass_inp" name="pass_inp" value={loginData.password} {...register('pass_inp',{required:"Password is required."})} onChange={(e)=>{setloginData((prev)=>{return{...prev,password:e.target.value}})}}  /><br/><small className='text-red-700'>{errors?.pass_inp && errors.pass_inp.message}</small></td>
+                    <td className='md:p-3 sm:p-1'><label htmlFor='pass_inp' className='font-medium md:text-2xl sm:text-lg'>Password</label></td>
+                    <td className='md:p-3 sm:p-1'><input className='rounded-xl p-1 md:p-1 sm:p-0 md:w-[400px] sm:w-[200px] text-black' type="text" id="pass_inp" name="pass_inp" value={loginData.password} {...register('pass_inp',{required:"Password is required."})} onChange={(e)=>{setloginData((prev)=>{return{...prev,password:e.target.value}})}}  /><br/><small className='text-red-700'>{errors?.pass_inp && errors.pass_inp.message}</small></td>
                 </tr>
 
                 <tr>
-                    <td className='p-3 text-center pt-10' colSpan={2}><button className='p-3 w-40 bg-blue-900 rounded-xl text-white font-medium hover:scale-110 cursot-pointer mr-2' type="reset">Reset</button><button className='p-3 ml-3 w-40 bg-teal-800 text-white font-medium rounded-xl hover:scale-110 cursot-pointer' >Submit</button></td>
+                    <td className='md:p-3 sm:pt-4 md:pt-10 text-center' colSpan={2}><button className='md:p-3 sm:p-1 md:w-40 sm:w-28 bg-blue-900 md:rounded-xl sm:rounded-lg text-white font-medium hover:scale-110 cursot-pointer mr-2 md:text-2xl sm:text-lg' type="reset">Reset</button><button className='md:p-3 sm:p-1 md:w-40 sm:w-28 ml-3 bg-teal-800 text-white font-medium md:rounded-xl sm:rounded-lg hover:scale-110 cursot-pointer  md:text-2xl sm:text-lg' >Submit</button></td>
                     
                 </tr>
             </tbody>
--- client/table_booking_app/src/components/Signup.js
+++ client/table_booking_app/src/components/Signup.js
@@ -46,36 +46,34 @@ function Signup() {
          fontSize:"24px"
      },
   }}/></div>
-    <div className="m-auto pt-40" style={{backgroundImage:`url(${bgImg})`,width:"100%",height:"1400px",backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
-        <div className='w-2/5 m-auto bg-stone-400 p-20 rounded-3xl' >
+    <div className="mx-auto md:pt-40 sm:pt-10 w-full md:h-[1400px] sm:h-[570px]" style={{backgroundImage:`url(${bgImg})`,width:"100%",backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
+        <div className='md:w-2/5 sm:w-[90%] mx-auto bg-stone-400 md:p-20 sm:p-4 rounded-3xl' >
             <form onSubmit={handleSubmit(getformData,onErros)}>
                <table className='text-2xl m-auto'>
                 <tbody>
                     <tr >
-                        <td className='p-3'><label htmlFor='name_inp' className='font-medium'>Name</label></td>
-                        <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="name_inp" name="name_inp" {...register('name_inp',{required:"Name is required"})} autoFocus onChange={(e)=>{setUserdata((prev)=>{return {...prev,name:e.target.value}})}} value={userData.name}/><br/><small className='text-red-700'>{errors?.name_inp && errors.name_inp.message}</small></td>
+                        <td className='md:p-3 sm:p-1'><label htmlFor='name_inp' className='font-medium md:text-2xl sm:text-lg'>Name</label></td>
+                        <td className='md:p-3 sm:p-1'><input className='rounded-xl md:p-1 sm:p-0 md:w-[400px] sm:w-[200px] text-black' type="text" id="name_inp" name="name_inp" {...register('name_inp',{required:"Name is required"})} autoFocus onChange={(e)=>{setUserdata((prev)=>{return {...prev,name:e.target.value}})}} value={userData.name}/><br/><small className='text-red-700'>{errors?.name_inp && errors.name_inp.message}</small></td>
                     </tr>
                          
                     <tr>
-                        <td className='p-3'><label htmlFor='email_inp' className='font-medium'>Email Id</label></td> 
-                        <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="email_inp" name="email_inp" {...register('email_inp',{required:"Email is required"})} onChange={(e)=>{setUserdata((prev)=>{return {...prev,email:e.target.value}})}} value={userData.email} /><br/><small className='text-red-700'>{errors?.email_inp && errors.email_inp.message}</small></td>
+                        <td className='md:p-3 sm:p-1'><label htmlFor='email_inp' className='font-medium md:text-2xl sm:text-lg'>Email Id</label></td> 
+                        <td className='md:p-3 sm:p-1'><input className='rounded-xl md:p-1 sm:p-0 md:w-[400px] sm:w-[200px] text-black' type="text" id="email_inp" name="email_inp" {...register('email_inp',{required:"Email is required"})} onChange={(e)=>{setUserdata((prev)=>{return {...prev,email:e.target.value}})}} value={userData.email} /><br/><small className='text-red-700'>{errors?.email_inp && errors.email_inp.message}</small></td>
                     </tr>
 
                     <tr>
-                        <td className='p-3'><label htmlFor='mob_inp' className='font-medium'>Mobile No</label></td>
-                        <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="text" id="mob_inp" name="mob_inp" {...register('mob_inp',{required:"Moble number is required."})}maxLength={10} onChange={(e)=>{setUserdata((prev)=>{return {...prev,mobno:e.target.value}})}} value={userData.mobno} /><br/><small className='text-red-700'>{errors?.mob_inp && errors.mob_inp.message}</small></td>
+                        <td className='md:p-3 sm:p-1'><label htmlFor='mob_inp' className='font-medium md:text-2xl sm:text-lg'>Mobile No</label></td>
+                        <td className='md:p-3 sm:p-1'><input className='rounded-xl md:p-1 sm:p-0 md:w-[400px] sm:w-[200px] text-black' type="text" id="mob_inp" name="mob_inp" {...register('mob_inp',{required:"Moble number is required."})}maxLength={10} onChange={(e)=>{setUserdata((prev)=>{return {...prev,mobno:e.target.value}})}} value={userData.mobno} /><br/><small className='text-red-700'>{errors?.mob_inp && errors.mob_inp.message}</small></td>
                     </tr>
 
                     <tr>
-                        <td className='p-3'><label htmlFor='password_inp' className='font-medium'>Password</label></td>
-                        {/* <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="password" id="password_inp" name="password_inp" {...register('password_inp',{required:"Password is required.",validate:{minLength: (v)=>v.length>=7 && v.length<=10,matchPattern:(v)=>/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/.test(v)}})} onChange={(e)=>{setUserdata((prev)=>{return {...prev,password:e.target.value}})}} value={userData.password} /><br/><small className='text-red-700'>{errors?.password_inp && errors.password_inp.message}</small></td> */}
-                        <td className='p-3'><input className='rounded-xl p-1 w-[400px] text-black' type="password" id="password_inp" name="password_inp" {...register('password_inp',{required:"Password is required.",validate:{minLength: (v)=>v.length>=7 && v.length<=10}})} onChange={(e)=>{setUserdata((prev)=>{return {...prev,password:e.target.value}})}} value={userData.password} /><br/><small className='text-red-700'>{errors?.password_inp && errors.password_inp.message}</small></td>
+                        <td className='md:p-3 sm:p-1'><label htmlFor='password_inp' className='font-medium md:text-2xl sm:text-lg'>Password</label></td>
+
+                        <td className='md:p-3 sm:p-1'><input className='rounded-xl md:p-1 sm:p-0 md:w-[400px] sm:w-[200px] text-black' type="password" id="password_inp" name="password_inp" {...register('password_inp',{required:"Password is required.",validate:{minLength: (v)=>v.length>=7 && v.length<=10}})} onChange={(e)=>{setUserdata((prev)=>{return {...prev,password:e.target.value}})}} value={userData.password} /><br/><small className='text-red-700'>{errors?.password_inp && errors.password_inp.message}</small></td>
                     </tr>
 
                     <tr>
-                        {/* <td className='p-3 text-center pt-10' colSpan={2}><button className='p-3 w-40 bg-blue-900 rounded-xl text-white font-medium hover:scale-110 cursot-pointer mr-2' type="reset">Reset</button><button className='p-3 ml-3 w-40 bg-teal-800 text-white font-medium rounded-xl hover:scale-110 cursot-pointer' onClick={getformData}>Submit</button></td> */}
-
-                        <td className='p-3 text-center pt-10' colSpan={2}><button className='p-3 w-40 bg-blue-900 rounded-xl text-white font-medium hover:scale-110 cursot-pointer mr-2' type="reset">Reset</button><button className='p-3 ml-3 w-40 bg-teal-800 text-white font-medium rounded-xl hover:scale-110 cursot-pointer'>Submit</button></td>
+                        <td className='p-3 text-center md:pt-10 sm:pt-4' colSpan={2}><button className='md:p-3 sm:p-1 md:w-40 sm:w-28 bg-blue-900 md:rounded-xl sm:rounded-lg text-white font-medium hover:scale-110 cursot-pointer mr-2 md:text-2xl sm:text-lg' type="reset">Reset</button><button className='md:p-3 sm:p-1 md:w-40 sm:w-28 ml-3 bg-teal-800 text-white font-medium md:rounded-xl sm:rounded-lg hover:scale-110 cursot-pointer  md:text-2xl sm:text-lg'>Submit</button></td>
                         
                     </tr>
                 </tbody>
