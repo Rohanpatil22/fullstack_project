9dd85f8446dbf679c629219bb1c4465f2754dbfe
Rohanpatil22 <rohanpatil1797@gmail.com>
2024-02-21 21:53:29 +0530

    unistalled cors packages and remoe cors

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import router from "./routes/routes.js";
 import cookieParser from "cookie-parser";
-import Cors from "cors";
+
 
 
 dotenv.config();
@@ -11,12 +11,7 @@ dotenv.config();
 const app=express();
 
 app.use(express.json());
-app.use(Cors(
-    {
-        origin:"https://65d61b10a53d28ca0d78c173--effervescent-cascaron-d4b467.netlify.app",
-        credentials:true
-    }
-));
+
 
 app.use(cookieParser());
 const PORT=process.env.PORT || 3000;
